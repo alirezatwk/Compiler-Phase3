37ee165ead6473ee3286230144726aaaed4faca4
Alireza Tavakoli <alirezata3akoli@gmail.com>
2021-01-01 16:32:44 +0330

    Improvement on structure.

--- Sophia-Phase3/src/main/visitor/typeChecker/TypeChecker.java
+++ Sophia-Phase3/src/main/visitor/typeChecker/TypeChecker.java
@@ -11,8 +11,17 @@ import main.ast.nodes.statement.loop.BreakStmt;
 import main.ast.nodes.statement.loop.ContinueStmt;
 import main.ast.nodes.statement.loop.ForStmt;
 import main.ast.nodes.statement.loop.ForeachStmt;
+import main.compileErrorException.typeErrors.*;
+import main.symbolTable.SymbolTable;
+import main.symbolTable.exceptions.ItemNotFoundException;
+import main.symbolTable.items.ClassSymbolTableItem;
+import main.symbolTable.items.MethodSymbolTableItem;
 import main.symbolTable.utils.graph.Graph;
+import main.symbolTable.utils.graph.exceptions.GraphDoesNotContainNodeException;
 import main.visitor.Visitor;
+
+import javax.swing.plaf.nimbus.State;
+import java.util.Collection;
 
 public class TypeChecker extends Visitor<Void> {
     private final Graph<String> classHierarchy;
@@ -26,38 +35,110 @@ public class TypeChecker extends Visitor<Void> {
 
     @Override
     public Void visit(Program program) {
-        //TODO
-        // TODO: update currentClass
+        SymbolTable.top = SymbolTable.root;
+        boolean hasMain = false;
+        for (ClassDeclaration classDeclaration : program.getClasses()) {
+            if (classDeclaration.getClassName().getName().equals("Main"))
+                hasMain = true;
+            classDeclaration.accept(this);
+        }
+        // Error 25
+        if (!hasMain)
+            program.addError(new NoMainClass());
         return null;
     }
 
     @Override
     public Void visit(ClassDeclaration classDeclaration) {
-        //TODO
+        try {
+            ExpressionTypeChecker.currentClass = classDeclaration;
+            ClassSymbolTableItem classSymbolTableItem = (ClassSymbolTableItem) SymbolTable.top.getItem(ClassSymbolTableItem.START_KEY + classDeclaration.getClassName().getName(), true);
+            SymbolTable.push(classSymbolTableItem.getClassSymbolTable());
+        } catch (ItemNotFoundException ignored) {
+        }
+
+        // Main
+        if (classDeclaration.getClassName().getName().equals("Main")) {
+            try {
+                Collection<String> collection = classHierarchy.getParentsOfNode(classDeclaration.getClassName().getName());
+                // Error 26
+                if (!collection.isEmpty())
+                    classDeclaration.addError(new MainClassCantExtend(classDeclaration.getLine()));
+
+                // Error 28
+                if (classDeclaration.getConstructor() != null)
+                    classDeclaration.addError(new NoConstructorInMainClass(classDeclaration));
+
+            } catch (GraphDoesNotContainNodeException ignored) {
+            } // Cause it can't.
+        } else {
+            // Other
+            // Error 27
+            if (classDeclaration.getParentClassName().getName().equals("Main"))
+                classDeclaration.addError(new CannotExtendFromMainClass(classDeclaration.getLine()));
+        }
+        if (classDeclaration.getConstructor() != null)
+            classDeclaration.getConstructor().accept(this);
+        for (MethodDeclaration methodDeclaration : classDeclaration.getMethods())
+            methodDeclaration.accept(this);
+        for (FieldDeclaration fieldDeclaration : classDeclaration.getFields())
+            fieldDeclaration.accept(this);
+
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(ConstructorDeclaration constructorDeclaration) {
-        //TODO
+        try {
+            MethodSymbolTableItem methodSymbolTableItem = (MethodSymbolTableItem) SymbolTable.top.getItem(MethodSymbolTableItem.START_KEY + constructorDeclaration.getMethodName().getName(), true);
+            SymbolTable.push(methodSymbolTableItem.getMethodSymbolTable());
+        } catch (ItemNotFoundException ignored) {
+        }
+
+        // Error 29
+        if (ExpressionTypeChecker.currentClass.getClassName().getName().equals("Main")) {
+            if (!constructorDeclaration.getArgs().isEmpty()) {
+                constructorDeclaration.addError(new MainConstructorCantHaveArgs(constructorDeclaration.getLine()));
+            }
+        }
+        for(VarDeclaration varDeclaration : constructorDeclaration.getArgs())
+            varDeclaration.accept(this);
+        for(VarDeclaration varDeclaration: constructorDeclaration.getLocalVars())
+            varDeclaration.accept(this);
+        for(Statement statement : constructorDeclaration.getBody())
+            statement.accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(MethodDeclaration methodDeclaration) {
-        //TODO
+        try {
+            MethodSymbolTableItem methodSymbolTableItem = (MethodSymbolTableItem) SymbolTable.top.getItem(MethodSymbolTableItem.START_KEY + methodDeclaration.getMethodName().getName(), true);
+            SymbolTable.push(methodSymbolTableItem.getMethodSymbolTable());
+        } catch (ItemNotFoundException ignored) {
+        }
+
+        for(VarDeclaration varDeclaration : methodDeclaration.getArgs())
+            varDeclaration.accept(this);
+        for(VarDeclaration varDeclaration: methodDeclaration.getLocalVars())
+            varDeclaration.accept(this);
+        for(Statement statement : methodDeclaration.getBody())
+            statement.accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(FieldDeclaration fieldDeclaration) {
-        //TODO
+        fieldDeclaration.getVarDeclaration().accept(this);
         return null;
     }
 
     @Override
     public Void visit(VarDeclaration varDeclaration) {
-        //TODO
+        
         return null;
     }
 
